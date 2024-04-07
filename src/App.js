dd9cab581bf6040c1674f2a9ddaee18ed973207d
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2024-04-07 19:31:20 +0530

    added chatbot api

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-center">
+    <iframe
+src="https://www.chatbase.co/chatbot-iframe/Fxw3XXe7Bi2W6KXdD7veI"
+
+// style="height: 100%; min-height: 700px"
+className="chat-iframe"
+frameborder="0"
+></iframe>
+    </div>
+    </div>
+  );
+}
+
+export default App;
